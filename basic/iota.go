7317401a75befc60b07b1414bb1b0ad4e1016e1d
Yuntwo <e1101579@u.nus.edu>
2024-12-10 09:31:50 +0800

    Init

---
+++ basic/iota.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+const (
+    i=1<<iota
+    j=3<<iota
+	// 省略赋值表达式，沿用上一行的表达式形式
+    k
+    l
+)
+
+func main() {
+    fmt.Println("i=",i)
+    fmt.Println("j=",j)
+    fmt.Println("k=",k)
+    fmt.Println("l=",l)
+}
