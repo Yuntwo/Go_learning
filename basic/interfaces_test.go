91513489da78c8850d75bd6a083b3d0e22459d6e
Lucy <yuntwoyuntow@gmail.com>
2025-04-04 23:01:31 +0800

    test: interface

--- basic/interfaces_test.go
+++ basic/interfaces_test.go
@@ -3,6 +3,7 @@ package basic
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"testing"
 )
 
@@ -24,4 +25,23 @@ func TestAbser_Abs(t *testing.T) {
 	//a = v
 
 	fmt.Println(a.Abs())
+}
+
+type Binary uint64
+
+// 没有方法的类型使用eface的底层数据结构存储
+func TestEface(t *testing.T) {
+	b := Binary(200)
+	// 这个类型转换使用了Go运行时（runtime包）将具体类型转换为interface{}的convT2E函数
+	// 核心逻辑是：
+	//func convT2E(t *_type, elem unsafe.Pointer) (e eface) {
+	//	e._type = t
+	//	e.data = elem
+	//	return
+	//}
+	// 这个过程保证了动态类型信息和数据能够正确存储到interface{}中
+	any := (interface{})(b)
+	fmt.Println(any)                 // 输出200
+	fmt.Println(reflect.TypeOf(b))   // basic.Binary
+	fmt.Println(reflect.TypeOf(any)) // basic.Binary
 }
